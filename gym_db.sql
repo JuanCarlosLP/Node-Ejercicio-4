da2d4e809248b885de5fc72bd8047152122efdc0
JuanCarlosLP <jclarios.js@gmail.com>
2021-01-20 22:56:06 -0600

    DB gym

--- gym_db.sql
+++ gym_db.sql
@@ -1,58 +1,69 @@
 create table client(
 	client_id serial not null,
-	"name" varchar(255) not null,
-	"lastname" varchar(255) not null,
-	email varchar(55) not null,
-	phone varchar(10) not null,
+	"firstName" varchar(255) not null,
+	"lastName" varchar(255) not null,
+	"email" varchar(55) not null,
+	"phone" varchar(10) not null,
 	"enable" boolean not null,
 	primary key (client_id)
 );
 
+create table membership(
+	membership_id serial not null,
+	"name" varchar(55) not null,
+	"price" money not null,
+	"period" varchar(55) not null,
+	"enable" boolean not null,
+	primary key (membership_id)
+);
+
 create table instructor(
 	instructor_id serial not null,
-	"name" varchar(255) not null,
-	"lastname" varchar(255) not null,
-	email varchar(55) not null,
+	"firstName" varchar(255) not null,
+	"lastName" varchar(255) not null,
+	"email" varchar(55) not null,
 	"available" boolean not null,
 	primary key (instructor_id)
 );
 
-create table payment(
-	payment_id serial not null,
-	client_id integer not null,
-	"date" timestamp not null,
-	payment_method varchar(55),
-	primary key (payment_id),
-	foreign key (client_id) references Client (client_id)
+create table schedule(
+	schedule_id serial not null,
+	"startTime" time not null,
+	"endTime" time not null
 );
 
-create table membership(
-	membership_id serial not null,
+create table payment(
+	payment_id serial not null,
+	membership_id integer not null,
 	client_id integer not null,
-	"type" varchar(55) not null,
-	"period" varchar(55) not null,
-    "price" money not null,
-	"enable" boolean not null,
-	primary key (membership_id),
-	foreign key (client_id) references Client (client_id)
+	"amount" money not null,
+	"date" timestamp not null,
+	primary key (payment_id),
+	foreign key (client_id) references client (client_id),
+	foreign key (membership_id) references membership (membership_id)
 );
 
 create table class(
 	class_id serial not null,
-    instructor_id integer not null,
-	capacity integer not null,
-    "time" time not null,
-    "date" date not null,
-    "class_name" varchar(55) not null,
+	instructor_id integer not null,
+	"className" varchar(55) not null,
+	"capacity" integer not null,
 	primary key (class_id),
     foreign key (instructor_id) references Instructor (instructor_id)
 );
 
-create table class_attendance(
-	class_id integer not null,
+create table clients_classes(
 	client_id integer not null,
-	attendance boolean not null,
-	primary key (class_id),
-	foreign key (class_id) references Class (class_id),
-	foreign key (client_id) references Client (client_id)
+	class_id integer not null,
+	primary key (client_id, class_id),
+	foreign key (client_id) references client (client_id),
+	foreign key (class_id) references class (class_id)
+);
+
+create table class_schedule(
+	schedule_id integer not null,
+	class_id integer not null,
+	"day" varchar(55) not null,
+	primary key (schedule_id, class_id),
+	foreign key (class_id) references class (class_id)
 );
--- seed_db.sql
+++ seed_db.sql
@@ -8,8 +8,23 @@ insert into client values(default, 'Eduardo', 'Rodriguez', 'edu34@gmail.com', '5
 						(default, 'Diana', 'Licea', 'dianalicea@gmail.com', '5513733253', true), 
 						(default, 'Diego', 'Martinez', 'diego.mar@gmail.com', '5513452189', true);
 
-insert into payment values(default, 1, '2021/1/2'),
-							(default, 4, '2020/4/5'), 
-							(default, 6, '2020/3/2'), 
-							(default, 7, '2020/7/21'), 
-							(default, 8, '2021/5/15');
+insert into membership values(default, 'Iron', 150, '1 mes', true), 
+							(default, 'Silver', 170, '3 meses', true), 
+							(default, 'Gold', 200, '6 meses', true), 
+							(default, 'Platinum', 250, '1 año', true), 
+							(default, 'basic', 200, '2 meses', false), 
+							(default, 'pro', 300, '1 año', false);
+						
+insert into instructor values(default, 'Hector', 'Silva', 'hector20@gmail.com', true),
+							(default, 'Ana', 'Licea', 'ana.licea@gmail.com', true), 
+							(default, 'Julio', 'Lopez', 'juliolopez@gmail.com', true), 
+							(default, 'Karla', 'Martinez', 'karla94@gmail.com', true), 
+							(default, 'Octavio', 'Herrera',	'octavioherrera@gmail.com', true), 
+							(default, 'Maria', 'Pereira', 'maria.pereira@gmail.com', true), 
+							(default, 'Fernanda', 'Ochoa',	'fernanda.ochoa@gmail.com', true);
+
+insert into payment values(default, 3, 1, 220, '2021/1/2'),
+							(default, 1, 4, 110, '2020/4/5'), 
+							(default, 2, 6, 195, '2020/3/2'), 
+							(default, 4, 7, 270, '2020/7/21'), 
+							(default, 4, 8, 270, '2021/5/15');
